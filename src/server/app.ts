f2b82af13fce427faf811dcf9f08979fd67689e5
antspring <80751041+antspring@users.noreply.github.com>
2026-07-06 04:12:40 +0500

    chore: add backend project structure

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,12 +1,16 @@
 import express from 'express';
 
 import { router } from '../routes';
+import { errorHandler } from '../shared/http/middlewares/errorHandler';
+import { notFoundHandler } from '../shared/http/middlewares/notFoundHandler';
 
 export const createApp = () => {
   const app = express();
 
   app.use(express.json());
   app.use(router);
+  app.use(notFoundHandler);
+  app.use(errorHandler);
 
   return app;
 };
