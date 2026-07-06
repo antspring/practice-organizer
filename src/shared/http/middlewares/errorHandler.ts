10d130b9880c7f99b06eccffd05e644c91d601ed
antspring <80751041+antspring@users.noreply.github.com>
2026-07-06 05:04:17 +0500

    feat: add auth routes

--- src/shared/http/middlewares/errorHandler.ts
+++ src/shared/http/middlewares/errorHandler.ts
@@ -1,8 +1,17 @@
 import { ErrorRequestHandler } from 'express';
+import { ZodError } from 'zod';
 
 import { AppError } from '../errors/AppError';
 
 const errorHandler: ErrorRequestHandler = (error, _request, response, _next) => {
+  if (error instanceof ZodError) {
+    response.status(400).json({
+      message: 'Validation error',
+      errors: error.issues,
+    });
+    return;
+  }
+
   if (error instanceof AppError) {
     response.status(error.statusCode).json({ message: error.message });
     return;
